b91430f2c0f496e8829cc293b7a8290db3818b8f
GitiL <GitiL@skyvar.co.il>
2024-08-06 13:06:17 +0300

    try internal

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = var.subscription_id
+}
+
 
 resource "azurerm_resource_group" "rgint" {
   name     = var.resource_group_int_name

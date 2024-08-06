dc13f348cdb3c13a00741e728159c9a6c38e90a0
GitiL <GitiL@skyvar.co.il>
2024-08-06 15:54:45 +0300

    try internal

--- main.tf
+++ main.tf
@@ -8,79 +8,83 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "rgint" {
-  name     = var.resource_group_int_name
-  location = var.resource_group_location
-}
+# resource "azurerm_resource_group" "rgint" {
+#   name     = var.resource_group_int_name
+#   location = var.resource_group_location
+# }
 
-resource "azurerm_virtual_network" "vnetint" {
-  name                = var.vnetint_name
+# resource "azurerm_virtual_network" "vnetint" {
+#   name                = var.vnetint_name
+#   location            = var.resource_group_location
+#   resource_group_name = azurerm_resource_group.rgint.name
+#   address_space       = ["10.3.0.0/16"]
+# }
+
+# resource "azurerm_subnet" "snetpgsql" {
+#   name                 = var.snetint_name
+#   resource_group_name = azurerm_resource_group.rgint.name
+#   virtual_network_name = azurerm_virtual_network.vnetint.name
+#   address_prefixes     = ["10.3.1.0/24"]
+#   delegation {
+#     name = "fs"
+#     service_delegation {
+#       name = "Microsoft.DBforPostgreSQL/flexibleServers"
+#       }
+#   }
+# }
+
+# resource "azurerm_private_dns_zone" "dnszonepgsql"  {
+#   name                = var.dns_zone_pg_name
+#   resource_group_name = azurerm_resource_group.rgint.name
+# }
+
+# resource "azurerm_private_dns_zone_virtual_network_link" "vlinkpgsql" {
+#   name                  = var.vnetlink_pg_name
+#   private_dns_zone_name = azurerm_private_dns_zone.dnszonepgsql.name
+#   virtual_network_id    = azurerm_virtual_network.vnetint.id
+#   resource_group_name =   azurerm_resource_group.rgint.name
+# }
+
+# resource "azurerm_virtual_network" "vnetext" {
+#   name                = var.vnetext_name
+#   location            = var.resource_group_location
+#   resource_group_name = azurerm_resource_group.rgint.name
+#   address_space       = ["10.2.0.0/16"]
+# }
+
+# resource "azurerm_subnet" "snetusersmanagement" {
+#   name                 = var.snetext_name
+#   resource_group_name = azurerm_resource_group.rgint.name
+#   virtual_network_name = azurerm_virtual_network.vnetext.name
+#   address_prefixes     = ["10.2.1.0/24"]
+# }
+
+# resource "azurerm_private_dns_zone" "dnszoneusersmanagement"  {
+#   name                = var.dns_zone_ext_name
+#   resource_group_name = azurerm_resource_group.rgint.name
+# }
+
+# resource "azurerm_private_dns_zone_virtual_network_link" "vlinkusersmanagement" {
+#   name                  = var.vnetlink_ext_name
+#   resource_group_name = azurerm_resource_group.rgint.name
+#   private_dns_zone_name = azurerm_private_dns_zone.dnszoneusersmanagement.name
+#   virtual_network_id    = azurerm_virtual_network.vnetext.id
+# }
+
+resource "azurerm_private_endpoint" "peext" {
+  name                = var.pe_usermanagement
   location            = var.resource_group_location
-  resource_group_name = azurerm_resource_group.rgint.name
-  address_space       = ["10.3.0.0/16"]
-}
+  resource_group_name = var.resource_group_int_name
+  subnet_id           = "/subscriptions/22710479-535c-4bc6-9d25-2194bd78372f/resourceGroups/rg-int/providers/Microsoft.Network/virtualNetworks/vnet-ext/subnets/snet-ext"
 
-resource "azurerm_subnet" "snetpgsql" {
-  name                 = var.snetint_name
-  resource_group_name = azurerm_resource_group.rgint.name
-  virtual_network_name = azurerm_virtual_network.vnetint.name
-  address_prefixes     = ["10.3.1.0/24"]
-  delegation {
-    name = "fs"
-    service_delegation {
-      name = "Microsoft.DBforPostgreSQL/flexibleServers"
-      }
+  private_service_connection {
+    name                           = var.pe_connection_name
+    private_connection_resource_id = "/subscriptions/a273b4fb-6a3d-4804-a047-5d293da8811d/resourcegroups/rg-ext/providers/Microsoft.Web/sites/wa-try-vnet"
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
   }
 }
 
-resource "azurerm_private_dns_zone" "dnszonepgsql"  {
-  name                = var.dns_zone_pg_name
-  resource_group_name = azurerm_resource_group.rgint.name
-}
 
-resource "azurerm_private_dns_zone_virtual_network_link" "vlinkpgsql" {
-  name                  = var.vnetlink_pg_name
-  private_dns_zone_name = azurerm_private_dns_zone.dnszonepgsql.name
-  virtual_network_id    = azurerm_virtual_network.vnetint.id
-  resource_group_name =   azurerm_resource_group.rgint.name
-}
 
-resource "azurerm_virtual_network" "vnetext" {
-  name                = var.vnetext_name
-  location            = var.resource_group_location
-  resource_group_name = azurerm_resource_group.rgint.name
-  address_space       = ["10.2.0.0/16"]
-}
 
-resource "azurerm_subnet" "snetusersmanagement" {
-  name                 = var.snetext_name
-  resource_group_name = azurerm_resource_group.rgint.name
-  virtual_network_name = azurerm_virtual_network.vnetext.name
-  address_prefixes     = ["10.2.1.0/24"]
-}
-
-resource "azurerm_private_dns_zone" "dnszoneusersmanagement"  {
-  name                = var.dns_zone_ext_name
-  resource_group_name = azurerm_resource_group.rgint.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "vlinkusersmanagement" {
-  name                  = var.vnetlink_ext_name
-  resource_group_name = azurerm_resource_group.rgint.name
-  private_dns_zone_name = azurerm_private_dns_zone.dnszoneusersmanagement.name
-  virtual_network_id    = azurerm_virtual_network.vnetext.id
-}
-
-# resource "azurerm_private_endpoint" "peext" {
-#   name                = var.pe_usermanagement
-#   location            = var.resource_group_location
-#   resource_group_name = azurerm_resource_group.rgint.name
-#   subnet_id           = azurerm_subnet.snetusersmanagement.id
-
-#   private_service_connection {
-#     name                           = var.pe_connection_name
-#     private_connection_resource_id = azurerm_linux_web_app.wausersmanagement.id
-#     is_manual_connection           = false
-#     subresource_names              = ["sites"]
-#   }
-# }
